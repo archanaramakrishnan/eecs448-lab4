dccc8a88379f5fd535f057208986399fcbc2263f
Archana Ramakrishnan <archana2359@gmail.com>
2019-10-06 15:33:03 -0500

    feat: added the first column of numbers from 1-100

--- exercise1/multiplication.php
+++ exercise1/multiplication.php
@@ -21,6 +21,7 @@ function countNum()
   for ($i = 1; $i <= 100; $i++)
   {
     echo "<tr>";
+    echo "<td>" . $i . "</td>";
     for ($j = 1; $j <= 100; $j++)
     {
       echo "<td>" . product($i, $j) . "</td>";
