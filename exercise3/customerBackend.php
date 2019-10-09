c655974d0ecec6819434f9924b5eb66fb3df53b9
Archana Ramakrishnan <archana2359@gmail.com>
2019-10-09 15:30:16 -0500

    bug: changed tag names such that they do not have spaces. this fixed getting the quantities in php

--- exercise3/customerBackend.php
+++ exercise3/customerBackend.php
@@ -11,14 +11,54 @@ function welcome() {
 }
 
 function receipt() {
-  $quantity1 = $_POST["Pair of Scissors"];
-  $quantity2 = $_POST["Elmer's Glue"];
-  $quantity3 = $_POST["Construction Paper"];
-  $quantity4 = $_POST["Sticky Notes"];
+  $quantity1 = $_POST["PairOfScissors"];
+  $quantity2 = $_POST["ElmersGlue"];
+  $quantity3 = $_POST["ConstructionPaper"];
+  $quantity4 = $_POST["StickyNotes"];
   $quantity5 = $_POST["Tape"];
 
   echo "<table>";
+  echo "<tr>";
+  echo "<td>" . "&nbsp" . "</td>";
+  echo "<td>" . "Quantity" . "</td>";
+  echo "<td>" . "Cost Per Item" . "</td>";
+  echo "<td>" . "Sub Total" . "</td>";
+  echo "</tr>";
 
+  echo "<tr>";
+  echo "<td>" . "Pair of Scissors" . "</td>";
+  echo "<td>" . $quantity1 . "</td>";
+  echo "<td>" . "$3.99" . "</td>";
+  echo "<td>$" . 3.99*$quantity1 . "</td>";
+  echo "</tr>";
+
+  echo "<tr>";
+  echo "<td>" . "Elmer's Glue" . "</td>";
+  echo "<td>" . $quantity2 . "</td>";
+  echo "<td>" . "$5.00" . "</td>";
+  echo "<td>$" . 5.00*$quantity2 . "</td>";
+  echo "</tr>";
+
+  echo "<tr>";
+  echo "<td>" . "Construction Paper" . "</td>";
+  echo "<td>" . $quantity3 . "</td>";
+  echo "<td>" . "$14.99" . "</td>";
+  echo "<td>$" . 14.99*$quantity3 . "</td>";
+  echo "</tr>";
+
+  echo "<tr>";
+  echo "<td>" . "Post-It Notes" . "</td>";
+  echo "<td>" . $quantity4 . "</td>";
+  echo "<td>" . "$3.88" . "</td>";
+  echo "<td>$" . 3.88*$quantity4 . "</td>";
+  echo "</tr>";
+
+  echo "<tr>";
+  echo "<td>" . "Tape" . "</td>";
+  echo "<td>" . $quantity5 . "</td>";
+  echo "<td>" . "$8.99" . "</td>";
+  echo "<td>$" . 8.99*$quantity5 . "</td>";
+  echo "</tr>";
   echo "</table>";
 }
 
