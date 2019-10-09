7582a0df8c44a2e699d75612addaeab272b2aff7
Archana Ramakrishnan <archana2359@gmail.com>
2019-10-09 14:53:11 -0500

    feat: added post getters for the quantities of all the items

--- exercise3/customerBackend.php
+++ exercise3/customerBackend.php
@@ -1,10 +1,27 @@
 <?php
 
-function grade() {
+function welcome() {
   $shippingMethod = $_POST["ship"];
+  $username = $_POST["username"];
+  $password = $_POST["userPassword"];
 
+  echo "Welcome " . $username . "!<br>";
+  echo "Password: " . $password . "<br>";
   echo "<br> Chosen shipping method: " . $shippingMethod . "<br>";
 }
 
-echo grade();
+function receipt() {
+  $quantity1 = $_POST["Pair of Scissors"];
+  $quantity2 = $_POST["Elmer's Glue"];
+  $quantity3 = $_POST["Construction Paper"];
+  $quantity4 = $_POST["Sticky Notes"];
+  $quantity5 = $_POST["Tape"];
+
+  echo "<table>";
+
+  echo "</table>";
+}
+
+echo welcome();
+echo receipt();
 ?>
