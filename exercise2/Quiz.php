6d6e9cfae4a0ba276d03c05bf60ffc416b7e8543
Archana Ramakrishnan <archana2359@gmail.com>
2019-10-10 09:17:50 -0500

    bug: fixed incorrectly pasted questions in both files

--- exercise2/Quiz.php
+++ exercise2/Quiz.php
@@ -7,19 +7,19 @@ function grade() {
   $a4 = $_POST["a4"];
   $a5 = $_POST["a5"];
 
-  echo "Question 1: Approximately, what is the population of planet Earth?";
+  echo "<b>Question 1: Approximately, what is the population of planet Earth?</b>";
   echo "<br> You answered: " . $a1 . "<br>" . "Correct answer: 7.5 Billion";
 
-  echo "<br><br> Question 2: Approximately, what is the population of planet Earth?";
+  echo "<br><br> <b>Question 2: Which of these is not one of the Seven Wonders of 2019?</b>";
   echo "<br> You answered: " . $a2 . "<br>" . "Correct answer: The Great Pyramid of Giza";
 
-  echo "<br><br> Question 3: Approximately, what is the population of planet Earth?";
+  echo "<br><br><b> Question 3: Which of the colors is in the middle of the seven colors of a rainbow?</b>";
   echo "<br> You answered: " . $a3 . "<br>" . "Correct answer: Green";
 
-  echo "<br><br> Question 4: Approximately, what is the population of planet Earth?";
+  echo "<br><br><b> Question 4: How many continents are there?</b>";
   echo "<br> You answered: " . $a4 . "<br>" . "Correct answer: 7";
 
-  echo "<br><br> Question 5: Approximately, what is the population of planet Earth?";
+  echo "<br><br> <b>Question 5: Which animal is the mascot of World Wildlife Fund (WWF)?</b>";
   echo "<br> You answered: " . $a5 . "<br>" . "Correct answer: Panda";
 
   $points = 0;
@@ -45,6 +45,7 @@ function grade() {
     $points++;
   }
 
+  echo  "<br><br> <b>Total Score: " . $points . "/5</b>";
   echo  "<br><br> <b>Score: " . ($points/5)*100 . "%</b>";
 }
 
